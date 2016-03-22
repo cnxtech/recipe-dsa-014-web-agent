b956b7e424f85be8f0a8cf435773038f699ca123
larry-kluger <larry.kluger@docusign.com>
2016-03-22 18:51:46 +0200

    Store port in app.locals.port

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 module.paths.unshift('.'); // find our belly button
+var app = require('app'),
+    port = process.env.PORT || 5000;
 
-console.log('Node path: ', process.env.NODE_PATH);
-
-var app = require('app');
-
-app.set('port', (process.env.PORT || 5000));
+app.locals.port = port; // store for later use
+app.set('port', port);
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
