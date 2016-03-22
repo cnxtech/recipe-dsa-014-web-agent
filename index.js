1ec557e06c3e0794b0ca8aa3398f6acd37a2791d
larry-kluger <larry.kluger@docusign.com>
2016-03-22 16:26:34 +0200

    rm node-modules. Fix startup

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-var express = require('express');
-var app = express();
+module.paths.unshift('.'); // find our belly button
+
+console.log('Node path: ', process.env.NODE_PATH);
+
+var app = require('app');
 
 app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
