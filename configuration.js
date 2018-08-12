48cafe41a7535ea926e1a79dae060e68c1d22a79
AvivSimDS <aviv.simionovici@docusign.com>
2018-08-12 22:20:43 +0300

    arx  to ds
    https://webagentdev.arx.com ==> https://webapp-dsa-devctr.docusign.net

--- configuration.js
+++ configuration.js
@@ -4,7 +4,8 @@
 module.exports = {
     app_name: 'r014_dsa_webagent',
     session_secret: "uiyhb3kn3245335355jhnnnmdpew[]",
-    dsa_webagent_server: 'https://webagentdev.arx.com', // url for the Web Agent server,
+    //dsa_webagent_server: 'https://webagentdev.arx.com', // url for the Web Agent server,
+    dsa_webagent_server: 'https://webapp-dsa-devctr.docusign.net', // url for the Web Agent server,
         // not for the DocuSign Signature Appliance itself.
     finish_url_path: "/signing", // the path part of the url
     doc1_file: 'images/example.pdf',
